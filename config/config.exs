036e0ed672f63293f5ed15075f51bcfdd4781269
Jason Axelson <jason.axelson@gmail.com>
2018-09-16 09:33:31 -1000

    Add exsync config to match my exsync pr

--- config/config.exs
+++ config/config.exs
@@ -16,6 +16,15 @@ config :play, :viewport, %{
   ]
 }
 
+case Mix.env() do
+  :dev ->
+    config :exsync,
+      reload_timeout: 75,
+      reload_callback: {GenServer, :call, [Play.Component.Nav, :reload_current_scene]}
+
+  _ -> nil
+end
+
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
 # by uncommenting the line below and defining dev.exs, test.exs and such.
