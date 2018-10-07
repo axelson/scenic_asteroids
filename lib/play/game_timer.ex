ab11cc8c12b1afd152e503a9419a47fc4641b9ae
Jason Axelson <jason.axelson@gmail.com>
2018-10-07 09:45:56 -1000

    Basic fps based animation working!

--- lib/play/game_timer.ex
+++ lib/play/game_timer.ex
@@ -1,27 +1,17 @@
 defmodule Play.GameTimer do
   @behaviour SchedEx.TimeScale
   @fps 60
+  @multiplier 1
 
   @impl true
   defdelegate now(timezone), to: SchedEx.IdentityTimeScale
 
   @impl true
   def speedup do
-    @fps / 1000
+    @fps / 1000 * @multiplier
   end
 
   def speed() do
     @fps
-  end
-
-  def delay do
-    @fps
-    #how many milliseconds in a second?
-    #1000 ms in a s
-    #1000 ms/s
-    #60 fps/s
-    #how many ms per frame (ms/f)
-    #60 fps/s = 0.06 f/ms
-    #16.667 ms/f
   end
 end
--- lib/scenes/asteroids.ex
+++ lib/scenes/asteroids.ex
@@ -9,6 +9,10 @@ defmodule Play.Scene.Asteroids do
   alias Scenic.Graph
   alias Play.Component.Nav
 
+  defmodule State do
+    defstruct [:t, :x, :graph, :last_run_time]
+  end
+
   # Steps
   # [x] Draw a circle to represent an asteroid
   # [x] Draw the circle empty
@@ -30,30 +34,42 @@ defmodule Play.Scene.Asteroids do
   def init(_, _opts) do
     Process.register(self(), __MODULE__)
     push_graph(@initial_graph)
-    schedule_animate()
+    schedule_animations()
 
-    IO.inspect(self(), label: "self()")
-    IO.puts "still 465?"
-
-    Play.TimersExperiment.init()
-
-    {:ok, %{graph: @initial_graph, t: 0, x: 110}}
+    initial_state = %State{graph: @initial_graph, t: 0, x: 110}
+    {:ok, initial_state}
   end
 
-  def handle_info(:animate, state) do
+  def handle_info({:animate, expected_run_time}, state) do
+    diff = time_diff(state, expected_run_time)
     %{graph: graph, t: t, x: x} = state
-    schedule_animate()
 
     # x = x - 1 / 4
-    x = x + :rand.uniform() * 2
+    # speed = 5 / s
+    # diff = 0.1s
+    # diff = 100 ms
+    # speed = 10 u/s
+    speed = 100
+    dist = case diff do
+              0 -> 0
+              diff -> speed / 1000 * diff
+            end
+
+    x = x + dist
 
     graph =
       graph
-      |> Graph.modify(:asteroid1, &circle(&1, 30, t: {x, 100 + t * 1}))
-      |> Graph.modify(:asteroid2, &circle(&1, 30, t: {x, 200 + t * 1}))
+      |> Graph.modify(:asteroid1, &circle(&1, 30, t: {x, 100}))
+      |> Graph.modify(:asteroid2, &circle(&1, 30, t: {x, 200}))
       |> push_graph()
 
-    {:noreply, %{state | t: t + 1, x: x, graph: graph}}
+    new_state = %{state | t: t + 1, x: x, graph: graph, last_run_time: expected_run_time}
+    {:noreply, new_state}
+  end
+
+  defp time_diff(state, expected_run_time) do
+    last_run_time = Map.get(state, :last_run_time) || expected_run_time
+    DateTime.diff(expected_run_time, last_run_time, :millisecond)
   end
 
   @impl Scenic.Scene
@@ -72,11 +88,19 @@ defmodule Play.Scene.Asteroids do
   end
 
   def handle_input(input, _, state) do
-    IO.inspect(input, label: "#{__MODULE__} ignoring input")
+    # IO.inspect(input, label: "#{__MODULE__} ignoring input")
     {:noreply, state}
   end
 
   # def handle_input(_, _, state), do: {:noreply, state}
 
-  defp schedule_animate(), do: Process.send_after(self(), :animate, 10)
+  defp schedule_animations() do
+    pid = self()
+    # Process.send_after(self(), :animate, 2)
+    func = fn expected_run_time ->
+      Process.send(pid, {:animate, expected_run_time}, [])
+    end
+
+    SchedEx.run_in(func, 1, repeat: true, time_scale: Play.GameTimer)
+  end
 end
