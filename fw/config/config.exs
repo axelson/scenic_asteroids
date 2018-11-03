8fd2dfe967607abc0956e4e6a6e03ad8ca63bd4d
Jason Axelson <jason.axelson@gmail.com>
2018-11-03 07:47:05 -1000

    Switch to nerves_init_gadget project
    Not as complicated as I thought from the installation instructions.
    Could have done this manually (maybe helped with a gen* command)

--- fw/config/config.exs
+++ fw/config/config.exs
@@ -15,7 +15,7 @@ config :nerves, :firmware, rootfs_overlay: "rootfs_overlay"
 # involved with firmware updates.
 
 config :shoehorn,
-  init: [:nerves_runtime],
+  init: [:nerves_runtime, :nerves_init_gadget],
   app: Mix.Project.config()[:app]
 
 # Use Ringlogger as the logger backend and remove :console.
@@ -23,6 +23,30 @@ config :shoehorn,
 # configuring ring_logger.
 
 config :logger, backends: [RingLogger]
+
+# Authorize the device to receive firmware using your public key.
+# See https://hexdocs.pm/nerves_firmware_ssh/readme.html for more information
+# on configuring nerves_firmware_ssh.
+
+key = Path.join(System.user_home!, ".ssh/id_rsa.pub")
+unless File.exists?(key), do:
+  Mix.raise("No SSH Keys found. Please generate an ssh key")
+
+config :nerves_firmware_ssh,
+  authorized_keys: [
+    File.read!(key)
+  ]
+
+# Configure nerves_init_gadget.
+# See https://hexdocs.pm/nerves_init_gadget/readme.html for more information.
+
+config :nerves_init_gadget,
+  ifname: "usb0",
+  address_method: :dhcpd,
+  mdns_domain: "nerves.local",
+  node_name: nil,
+  node_host: :mdns_domain,
+  ssh_console_port: 22
 
 # Import target specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
--- fw/mix.exs
+++ fw/mix.exs
@@ -49,7 +49,8 @@ defmodule Fw.MixProject do
 
   defp deps(target) do
     [
-      {:nerves_runtime, "~> 0.6"}
+      {:nerves_runtime, "~> 0.6"},
+      {:nerves_init_gadget, "~> 0.4"}
     ] ++ system(target)
   end
 
