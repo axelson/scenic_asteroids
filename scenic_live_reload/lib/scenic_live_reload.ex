101693c07de9cedd4ac3cfb9d3a6feaaffe4d09d
Jason Axelson <jason.axelson@gmail.com>
2019-03-29 06:51:40 -1000

    Initial ScenicLiveReload app

---
+++ scenic_live_reload/lib/scenic_live_reload.ex
@@ -0,0 +1,18 @@
+defmodule ScenicLiveReload do
+  @moduledoc """
+  Documentation for ScenicLiveReload.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ScenicLiveReload.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
