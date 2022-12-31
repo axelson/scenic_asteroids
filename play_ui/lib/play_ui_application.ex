aabc0a3a980277b984d05228b246610043e70ee7
Jason Axelson <jason.axelson@gmail.com>
2022-12-31 09:16:23 -1000

    Scenic live reload update

--- play_ui/lib/play_ui_application.ex
+++ play_ui/lib/play_ui_application.ex
@@ -6,12 +6,7 @@ defmodule PlayUiApplication do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
-    # load the viewport configuration from config
-    main_viewport_config = Application.get_env(:play, :viewport)
-
-    children = [
-      {ScenicLiveReload, viewports: [main_viewport_config]}
-    ]
+    children = []
 
     Supervisor.start_link(children, strategy: :one_for_one)
   end
